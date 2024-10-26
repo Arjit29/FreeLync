35bdde180af712893b2c1e4df47cd862f569ca38
Arjit29 <arjit.bhardwaj2004@gmail.com>
2024-10-26 16:19:40 +0530

    Landing page added the images and background styles

---
+++ FreeLync/src/components/Landing/Landing.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Landing.css";
+
+export default function Landing(){
+    return (
+        <div className="bg-color">
+            <div className="first-container">
+                <div className="img1">
+
+                </div>
+                <div className="text">
+                    <h1 className="typing-text">Show the World what you are!!</h1>
+                </div>
+            </div>
+
+            <div className="second-container">
+                <div className="text">
+                    <h1 className="typing-text">Chance to utilize your creative freedom</h1>
+                </div>
+                <div className="img2">
+
+                </div>
+            </div>
+
+            <div className="third-container">
+                <div className="img3">
+
+                </div>
+                <div className="text">
+                    <h1 className="typing-text">Hire the Best</h1>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
--- FreeLync/src/main.jsx
+++ FreeLync/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./index.css"
+import Landing from './components/Landing/Landing.jsx'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Landing/>
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
